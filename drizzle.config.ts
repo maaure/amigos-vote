e56a34df4fa5d7e3f03224f867d980069eab29ba
Maure Andrade <pedro.maure@gmail.com>
2026-07-15 20:13:58 -0300

    feat: remove dependenicas do supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,14 +3,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
 
-const dbUrl =
-  process.env.DB_CLIENT === "supabase"
-    ? process.env.DATABASE_URL_SUPABASE_SESSION || process.env.DATABASE_URL_SUPABASE
-    : `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_ADDRESS}:5432/${process.env.POSTGRES_DB}`;
-
-if (!dbUrl) {
-  throw new Error("DATABASE_URL não está definida nas variáveis de ambiente.");
-}
+const dbUrl = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_ADDRESS}:5432/${process.env.POSTGRES_DB}`;
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
